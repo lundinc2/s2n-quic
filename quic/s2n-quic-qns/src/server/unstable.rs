85237af3629aa2adb586f94c9b1f2c49be169e5c
toidiu <apoorv@toidiu.com>
2022-03-21 17:08:23 -0700

    sleep

--- quic/s2n-quic-qns/src/server/unstable.rs
+++ quic/s2n-quic-qns/src/server/unstable.rs
@@ -3,11 +3,15 @@
 
 use core::task::Poll;
 use s2n_quic::provider::tls::s2n_tls::{ClientHelloHandler, Connection};
+use std::time::Duration;
+use tokio::time::Sleep;
 
 pub struct MyClientHelloHandler {}
 
 impl ClientHelloHandler for MyClientHelloHandler {
     fn poll_client_hello(&self, connection: &mut Connection) -> core::task::Poll<Result<(), ()>> {
-        Poll::Ready(Ok(()))
+        let sleep = tokio::time::sleep(Duration::from_secs(1));
+        let context = core::task::Context::from_waker(con.waker());
+        Box::pin(sleep).poll_unpin(&mut context).map(|_| Ok(()))
     }
 }
